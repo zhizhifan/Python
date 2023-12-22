267fd32dd6b084e1a72dd01856ceb236798e6657
zhizhifan <2120212517@stu.nufe.edu.cn>
2023-12-22 19:27:01 +0800

    changes

---
+++ Basis_notes/test.py
@@ -0,0 +1,12 @@
+# class Person:
+#   def __init__(self, name, age):
+#     self.name=name
+#     self.age=age
+
+#   @classmethod
+#   def cm(cls):
+    # self.show()
+
+# per=Person()
+
+# print(type(per))
